26685dd81a00ad296c1d5396487bea7dcb5eef8f
charles3399 <bernaldezsay@gmail.com>
2022-05-01 07:18:00 +0800

    Added some positions in positionseeder

--- database/seeders/PositionSeeder.php
+++ database/seeders/PositionSeeder.php
@@ -17,27 +17,50 @@ class PositionSeeder extends Seeder
     {
         Position::insert([
             'position_name' => 'Junior Web Developer',
-            'basic_pay' => 121
+            'basic_pay' => 121,
+            'created_at' => now()
         ]);
 
         Position::insert([
             'position_name' => 'Senior Web Developer',
-            'basic_pay' => 400
+            'basic_pay' => 400,
+            'created_at' => now()
         ]);
 
         Position::insert([
             'position_name' => 'Quality Assurance Analyst',
-            'basic_pay' => 250
+            'basic_pay' => 250,
+            'created_at' => now()
         ]);
 
         Position::insert([
             'position_name' => 'Project Manager',
-            'basic_pay' => 500
+            'basic_pay' => 500,
+            'created_at' => now()
         ]);
 
         Position::insert([
             'position_name' => 'Human Resource Staff',
-            'basic_pay' => 300
+            'basic_pay' => 300,
+            'created_at' => now()
+        ]);
+
+        Position::insert([
+            'position_name' => 'Graphic Designer',
+            'basic_pay' => 100,
+            'created_at' => now()
+        ]);
+
+        Position::insert([
+            'position_name' => 'Database Administrator',
+            'basic_pay' => 250,
+            'created_at' => now()
+        ]);
+
+        Position::insert([
+            'position_name' => 'System Analyst',
+            'basic_pay' => 150,
+            'created_at' => now()
         ]);
     }
 }
